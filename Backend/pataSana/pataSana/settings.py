066394a4dc5eadb66a8e58c7c5da8fef50b0aca3
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-06-17 20:08:03 -0500

    actualizar settings para deploy

--- Backend/pataSana/pataSana/settings.py
+++ Backend/pataSana/pataSana/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9dh%re(3#tt)ri3i5mo05z*-bu^-)moazmbv*s#%5v#8l92i$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"] 
 
 
 # Application definition
