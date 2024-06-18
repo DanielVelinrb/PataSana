3bac85d02774594abf5424894ec79fcec7680477
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-06-18 14:46:48 -0500

    actualizacion requirements

--- Backend/pataSana/pataSana/settings.py
+++ Backend/pataSana/pataSana/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9dh%re(3#tt)ri3i5mo05z*-bu^-)moazmbv*s#%5v#8l92i$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"] 
+ALLOWED_HOSTS = ["*"] 
 
 
 # Application definition
