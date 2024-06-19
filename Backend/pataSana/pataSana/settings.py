0e0fd3e257b4188d1eed73ad7b325b547cedb058
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2024-06-18 21:52:29 -0500

    Ultimo try vercel

--- Backend/pataSana/pataSana/settings.py
+++ Backend/pataSana/pataSana/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pataSana.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.dirname(os.path.dirname(BASE_DIR)) +'\Frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
